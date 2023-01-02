3a35783b7cc71f77086ca095b8e0f8208a62a003
HyunKyuLee <fosa152@gmail.com>
2023-01-02 21:30:11 +0900

    자바 27일차 : 채팅 프로그램

---
+++ java/src/day27/EX01_Client.java
@@ -0,0 +1,91 @@
+package day27;
+
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.net.InetSocketAddress;
+import java.net.Socket;
+import java.util.Scanner;
+
+public class EX01_Client {
+	
+	public static void main(String[] args) {
+		Socket socket = null;
+		
+		try {
+			socket = new Socket();
+			System.out.println("[연결 시도 중]");
+			socket.connect(new InetSocketAddress("서버 IP 주소",5001));
+			System.out.println("[연결 성공]");
+			send(socket,"학생");
+			recieve(socket);
+		} catch(Exception e) {
+			System.out.println("[연결 실패]");
+		}
+	}
+	public static void recieve(Socket socket) {
+		Thread t = new Thread(()->{
+			InputStream is = null;
+			try {
+				byte [] bytes = new byte[1024];
+				is = socket.getInputStream();
+				
+				while(true) {
+					int readCount = is.read(bytes);
+					if(readCount == -1)
+						break;
+					String str = new String(bytes, 0, readCount, "UTF-8");
+					int index = str.indexOf(",");
+					if(index == -1)
+						continue;
+					String name = str.substring(0, index);
+					String message = str.substring(index+1);
+					System.out.println(name + ">" + message);
+				}
+				is.close();
+			} catch(Exception e) {
+				
+			} finally {
+				try {
+					if(socket != null && !socket.isClosed())
+						socket.close();
+					System.out.println("[클라이언트 종료]");
+				} catch(Exception e2) {
+					System.out.println("[클라이언트 소켓 닫기 실패]");
+				}
+			}
+		});
+		t.start();
+	}
+	public static void send(Socket socket, String name) {
+		Thread t = new Thread(()->{
+			Scanner sc = new Scanner(System.in);	
+			try {
+				OutputStream os = socket.getOutputStream();
+				while(true) {
+					String str = sc.nextLine();
+					if(str.equals("exit")) {
+						if(socket != null && socket.isClosed())
+							socket.close();
+						os.close();
+						break;
+					}
+					str = name+","+str;
+					byte [] bytes = str.getBytes("UTF-8");
+					os.write(bytes);
+					os.flush();
+				}
+			} catch(Exception e) {
+				
+			} finally {
+				System.out.println("종료");
+				try {
+					if(socket != null && socket.isClosed())
+						socket.close();
+				} catch(Exception e) {
+					System.out.println("클라이언트 소켓 종료 실패");
+				}
+			}
+		});
+		t.start();
+	}
+}
