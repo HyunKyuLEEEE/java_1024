e78cc4e1748a7bee86d6b8d888092e118f478ecf
HyunKyuLee <fosa152@gmail.com>
2023-01-02 21:15:07 +0900

    자바 19일차 : 람다식 - Predicate 예제

---
+++ java/src/day19/PredicateEx01.java
@@ -0,0 +1,46 @@
+package day19;
+
+import java.util.ArrayList;
+import java.util.function.Predicate;
+
+public class PredicateEx01 {
+
+	public static void main(String[] args) {
+		ArrayList<Student> list = new ArrayList<Student>();
+		list.add(new Student(1,1,1, "홍길동",100,90,80));
+		list.add(new Student(1,2,1, "임꺽정",90,90,80));
+		list.add(new Student(1,1,2, "고길동",10,90,20));
+		/* 1학년 1반 학생들을 출력하는 코드를 작성하세요.
+		 * 1학년 2반 학생들을 출력하는 코드를 작성하세요.
+		 * 1학년 학생들을 출력하는 코드를 작성하세요.
+		 * */
+//		print(list, 1, 1);
+//		print(list, 1);
+//		print(list, (s)->s.getGrade() ==1 && s.getClassNum() == 1);
+		print(list, (s)->s.getGrade() ==1);
+		print(list, (s)->s.getName().charAt(0) == '홍');
+		
+	}
+	
+	public static void print(ArrayList<Student> list, int grade, int classNum) {
+		for(Student temp : list) {
+			if(temp.getGrade() == grade && temp.getClassNum() == classNum) {
+				System.out.println(temp);
+			}
+		}
+	}
+	public static void print(ArrayList<Student> list, int grade) {
+		for(Student temp : list) {
+			if(temp.getGrade() == grade) {
+				System.out.println(temp);
+			}
+		}
+	}
+	public static void print(ArrayList<Student> list, Predicate<Student> p) {
+		for(Student temp : list) {
+			if(p.test(temp)) {
+				System.out.println(temp);
+			}
+		}
+	}
+}
