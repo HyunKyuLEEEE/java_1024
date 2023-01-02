86411bd3cbf1a9643a54565d1f03903b7ed48e1e
HyunKyuLee <fosa152@gmail.com>
2023-01-02 20:22:29 +0900

    자바 03일차 : for문 예제 -1부터 10까지 합 예제

---
+++ java/src/day03/ForEx05.java
@@ -0,0 +1,20 @@
+package day03;
+
+public class ForEx05 {
+
+	public static void main(String[] args) {
+		/* 1부터 10까지의 합을 구하는 코드를 작성하세요.
+		 * 반복횟수 : i는 1부터 10까지 1씩 증가 
+		 * 규칙성 : sum = sum + i
+		 * 반복문 종료 후 : sum을 출력
+		 * */
+		
+		int i, sum = 0;
+		for(i = 0, sum = 0; i <= 10; i++) {
+			sum += i;
+		}
+		System.out.println(sum);
+		
+	}
+
+}
