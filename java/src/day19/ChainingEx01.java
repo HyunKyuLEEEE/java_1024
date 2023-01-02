03f45d435d32ed48dcb437a559350af3c3767777
HyunKyuLee <fosa152@gmail.com>
2023-01-02 21:14:44 +0900

    자바 19일차 : 체이닝 예제

---
+++ java/src/day19/ChainingEx01.java
@@ -0,0 +1,31 @@
+package day19;
+
+public class ChainingEx01 {
+
+	public static void main(String[] args) {
+		/* 체이닝 : .이 연달아 나와 메소드를 연속해서 호출
+		 * 앞에 있는 메소드 리턴 값(객체)에 이어서 메소드를 호출
+		 * */
+		String str = "Hello";
+		/* 문자열 str에 2번지부터 부분 문자열을 추출한 후, 0번지 문자를 가져옴
+		 * 0번지 문자를 가져왔기 때문에 charAt()다음에는 .을 통해 메소드를 호출할 수 없음*/
+		System.out.println(str.substring(2).charAt(0));
+		
+		System.out.println(1);
+		
+		//앞에서 작업한 리턴값에 이어 작업하는거기 때문에
+		//객체가 와야합니다.
+		System.out.println(toString(100).charAt(0));
+		
+		//print()는 리턴타입이 없기 때문에 체이닝을 쓸 수 없다.
+		//리턴타입이 일반 변수(int, char 등)인 경우도 체이닝을 쓸 수 없다.
+		print(10); // => X
+	}
+	
+	public static String toString(int num) {
+		return num + "";
+	}
+	public static void print(int num) {
+		System.out.println(num);
+	}
+}
